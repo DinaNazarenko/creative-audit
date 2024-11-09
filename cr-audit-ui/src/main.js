9ce497c71c76012a0d6e45b06ffb4b7a8457c60a
unknown <dinanazarenko@gmail.com>
2024-11-09 11:54:45 +0800

    switching to webpack

--- cr-audit-ui/src/main.js
+++ cr-audit-ui/src/main.js
@@ -3,10 +3,12 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import { autoAnimatePlugin } from "@formkit/auto-animate/vue";
-
+import axios from 'axios'
 
 import App from './App.vue'
 import router from './router'
+
+axios.defaults.baseURL = 'http://localhost:4000'
 
 const app = createApp(App)
 
