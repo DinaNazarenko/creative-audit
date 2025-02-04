163c24f84072f6f24e40e62497b3692fb6a17a03
unknown <dinanazarenko@gmail.com>
2025-02-04 12:34:15 +0800

    btn header

--- cr-audit-ui/src/components/CreativePage/HeaderInfo.vue
+++ cr-audit-ui/src/components/CreativePage/HeaderInfo.vue
@@ -1,5 +1,6 @@
 <script setup>
 import CreativeStatus from '@/components/common/CreativeStatus.vue'
+import ButtonChange from '@/components/common/ButtonChange.vue'
 import { GROUP_FIELDS } from '@/lib/constants'
 import { usePopover } from '@/lib/utils/popover'
 import { computed, nextTick, watch } from 'vue'
@@ -26,77 +27,79 @@ watch(
 )
 </script>
 <template>
-  <div class="d-flex">
-    <div class="info_item_min">
-      <p class="text-gray-600 m-0 fs_custom">
-        {{ GROUP_FIELDS.idApplication }}
-      </p>
-      <p class="fw-bold m-0 pe-3">{{ creative.idApplication }}</p>
-    </div>
-    <div class="info_item_min">
-      <p class="text-gray-600 m-0 fs_custom">{{ GROUP_FIELDS.status }}</p>
-      <p class="fw-bold m-0 pe-3">
-        <CreativeStatus :status="creative.status" />
-      </p>
-    </div>
-    <div class="info_item_min">
-      <p class="text-gray-600 m-0 fs_custom">{{ GROUP_FIELDS.type }}</p>
-      <p class="fw-bold m-0 pe-3">{{ creative.type }}</p>
-    </div>
-    <div class="info_item_max">
-      <p class="text-gray-600 m-0 fs_custom">{{ GROUP_FIELDS.email }}</p>
-      <p
-        class="fw-bold m-0 pe-3 info_item_max text-truncate"
-        data-bs-toggle="popover"
-        data-bs-trigger="hover focus"
-        data-bs-placement="top"
-        data-bs-delay="300"
-        data-bs-animation="true"
-        :data-bs-content="creative.email"
-      >
-        {{ creative.email }}
-      </p>
-    </div>
-    <div class="info_item_min">
-      <p class="text-gray-600 m-0 fs_custom">{{ GROUP_FIELDS.account }}</p>
-      <p
-        class="fw-bold m-0 pe-3 info_item_min text-truncate"
-        data-bs-toggle="popover"
-        data-bs-trigger="hover focus"
-        data-bs-placement="top"
-        data-bs-delay="300"
-        data-bs-animation="true"
-        :data-bs-content="creative.account"
-      >
-        {{ creative.account }}
-      </p>
-    </div>
-    <div class="info_item_max">
-      <p class="text-gray-600 m-0 fs_custom">{{ GROUP_FIELDS.advertiser }}</p>
-      <p
-        class="fw-bold m-0 pe-3 info_item_max text-truncate"
-        data-bs-toggle="popover"
-        data-bs-trigger="hover focus"
-        data-bs-placement="top"
-        data-bs-delay="300"
-        data-bs-animation="true"
-        :data-bs-content="creative.advertiser"
-      >
-        {{ creative.advertiser }}
-      </p>
-    </div>
-    <div class="info_item_min">
-      <p class="text-gray-600 m-0 fs_custom">
-        {{ GROUP_FIELDS.timeBeforeStart }}
-      </p>
-      <p class="fw-bold m-0 pe-3">
-        {{ creative?.timeBeforeStart ? `${creative?.timeBeforeStart} д` : '—' }}
-      </p>
+  <div class="d-flex justify-content-between">
+    <div class="d-flex">
+      <div class="info_item_min">
+        <p class="text-gray-600 m-0 fs_custom">
+          {{ GROUP_FIELDS.idApplication }}
+        </p>
+        <p class="fw-bold m-0 pe-3">{{ creative.idApplication }}</p>
+      </div>
+      <div class="info_item_min">
+        <p class="text-gray-600 m-0 fs_custom">{{ GROUP_FIELDS.status }}</p>
+        <p class="fw-bold m-0 pe-3">
+          <CreativeStatus :status="creative.status" />
+        </p>
+      </div>
+      <div class="info_item_min">
+        <p class="text-gray-600 m-0 fs_custom">{{ GROUP_FIELDS.type }}</p>
+        <p class="fw-bold m-0 pe-3">{{ creative.type }}</p>
+      </div>
+      <div class="info_item_max">
+        <p class="text-gray-600 m-0 fs_custom">{{ GROUP_FIELDS.email }}</p>
+        <p
+          class="fw-bold m-0 pe-3 info_item_max text-truncate"
+          data-bs-toggle="popover"
+          data-bs-trigger="hover focus"
+          data-bs-placement="top"
+          data-bs-delay="300"
+          data-bs-animation="true"
+          :data-bs-content="creative.email"
+        >
+          {{ creative.email }}
+        </p>
+      </div>
+      <div class="info_item_min">
+        <p class="text-gray-600 m-0 fs_custom">{{ GROUP_FIELDS.account }}</p>
+        <p
+          class="fw-bold m-0 pe-3 info_item_min text-truncate"
+          data-bs-toggle="popover"
+          data-bs-trigger="hover focus"
+          data-bs-placement="top"
+          data-bs-delay="300"
+          data-bs-animation="true"
+          :data-bs-content="creative.account"
+        >
+          {{ creative.account }}
+        </p>
+      </div>
+      <div class="info_item_max">
+        <p class="text-gray-600 m-0 fs_custom">{{ GROUP_FIELDS.advertiser }}</p>
+        <p
+          class="fw-bold m-0 pe-3 info_item_max text-truncate"
+          data-bs-toggle="popover"
+          data-bs-trigger="hover focus"
+          data-bs-placement="top"
+          data-bs-delay="300"
+          data-bs-animation="true"
+          :data-bs-content="creative.advertiser"
+        >
+          {{ creative.advertiser }}
+        </p>
+      </div>
+      <div class="info_item_min">
+        <p class="text-gray-600 m-0 fs_custom">
+          {{ GROUP_FIELDS.timeBeforeStart }}
+        </p>
+        <p class="fw-bold m-0 pe-3">
+          {{
+            creative?.timeBeforeStart ? `${creative?.timeBeforeStart} д` : '—'
+          }}
+        </p>
+      </div>
     </div>
     <div v-if="showsButton">
-      <button class="btn btn-primary btn_custom">
-        Показать все комментарии
-      </button>
+      <ButtonChange title="Показать все комментарии" />
     </div>
   </div>
 </template>
@@ -118,11 +121,5 @@ watch(
 }
 .fs_custom {
   font-size: 12px;
-}
-.btn_custom {
-  background-color: var(--custom-color) !important;
-  border-color: var(--custom-color) !important;
-  color: white !important;
-  border-radius: 4px;
 }
 </style>
